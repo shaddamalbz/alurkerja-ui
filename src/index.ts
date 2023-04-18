f5debb71fb48f2444df47772ba35d2171518bf65
shaddamalbz <shaddam.albz@gmail.com>
2023-04-18 13:03:41 +0700

    v0.0.2

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,32 @@ import {
   Badge,
   Button,
   CloseButton,
+  Dropdown,
+  Input,
+  Modal,
+  Select,
   HamburgerButton,
   Skeleton,
   Spinner,
   StatusIcon,
   Tag,
 } from '@/components/ui/'
+import { AlurkerjaForm, AlurkerjaTable } from '@/components/alurkerja'
 
 export {
   Avatar,
   Badge,
   Button,
   CloseButton,
+  Dropdown,
+  Input,
+  Modal,
+  Select,
   HamburgerButton,
   Skeleton,
   Spinner,
   StatusIcon,
   Tag,
+  AlurkerjaTable,
+  AlurkerjaForm,
 }
