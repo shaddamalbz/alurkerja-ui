2c0547e2b5d9d4c0b24924bc4b3e56f403236386
shaddamalbz <shaddam.albz@gmail.com>
2023-05-12 14:48:36 +0700

    [refac] remove closeButton Components

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,7 +1,6 @@
 import Avatar from './Avatar'
 import Badge from './Badge'
 import Button, { HamburgerButton } from './Button'
-import CloseButton from './CloseButton'
 import Skeleton from './Skeleton'
 import Spinner from './Spinner'
 import StatusIcon from './StatusIcon'
@@ -20,7 +19,6 @@ export {
   Badge,
   Button,
   HamburgerButton,
-  CloseButton,
   Skeleton,
   Spinner,
   StatusIcon,
