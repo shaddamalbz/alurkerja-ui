65f900d6ec1242952d9b2cb5f9264182ce2f08b4
shaddamalbz <shaddam.albz@gmail.com>
2023-05-17 16:48:41 +0700

    [docs] update TableLowcode stories

--- src/stories/components/alurkerja/Table/TableLowcode.stories.tsx
+++ src/stories/components/alurkerja/Table/TableLowcode.stories.tsx
@@ -3,10 +3,8 @@ import { TableLowcode } from '@/components/alurkerja'
 import { useState } from 'react'
 
 const meta = {
-  title: 'Components/Alurkerja/TableLowcode',
+  title: 'Features/TableLowcode',
   component: TableLowcode,
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
-  tags: ['autodocs'],
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'fullscreen',
@@ -151,7 +149,24 @@ export const CustomFilterField: Story = {
   },
   parameters: {
     docs: {
-      source: { language: 'tsx' },
+      source: {
+        code: `
+        const [pageConfig, setPageConfig] = useState({ limit: 10, page: 0 })
+        const [renderState, setRenderState] = useState(0)
+        const [filterBy, setFilterBy] = useState<{ [x: string]: any }>()
+        const [search, setSearch] = useState<string>()
+
+        <TableLowcode 
+          renderState={renderState}
+          setRenderState={setRenderState}
+          pageConfig={pageConfig}
+          setPageConfig={setPageConfig}
+          filterBy={filterBy}
+          setFilterBy={setFilterBy}
+          search={search}
+          setSearch={setSearch}
+        />`,
+      },
     },
   },
   render: (args) => {
